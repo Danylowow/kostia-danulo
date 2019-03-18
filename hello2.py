61531429304cf6f0cbbc1527258886e6b72cb733
Danylowow <48218211+Danylowow@users.noreply.github.com>
2019-03-18 22:37:22 +0200

    Delete hello2.py

--- hello2.py
+++
@@ -1,5 +0,0 @@
-from hello import encoder_or_decoder,output
-x = input("vvedit text dlya zakoduvanya abo rozkoduvanya:")
-z = input('zakoduvatu chu rozkoduvatu:')
-encoder_or_decoder(x,z)
-output('hello')
